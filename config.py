6e90627550a9906c11b139817b2156dad5a81965
Hashim463 <mohammedhashim1463@gmail.com>
2026-07-07 16:00:58 +0530

    Update default MySQL password in config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     # --- MySQL ---
     MYSQL_HOST = os.environ.get("MYSQL_HOST", "localhost")
     MYSQL_USER = os.environ.get("MYSQL_USER", "root")
-    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "hashim2004")
+    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "your_mysql_password")
     MYSQL_DB = os.environ.get("MYSQL_DB", "smart_attendance")
 
     # --- Face recognition ---
